090d800becc0d52a96f5374ba1e3ba05b0440015
mknsrp <krawczykmaciekit@gmail.com>
2025-09-17 18:00:33 +0200

    todo-app.py - nauka o listach

--- todo-app.py
+++ todo-app.py
@@ -1,3 +1,9 @@
-prompt = "Enter a todo: "
-text = input(prompt)
-print(text)
+user_prompt = "Enter a todo:
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+todos = [todo1, todo2, todo3]
+print(todos)
+
+print(type(todos))
