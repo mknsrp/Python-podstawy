607d5229e7f81a985236586f3e007a666073ac44
mknsrp <krawczykmaciekit@gmail.com>
2025-09-20 13:56:36 +0200

    Dodanie pliku bonus4.py - nauka o niezmienności ciągu znaków i zmienności list, niezmienna lista - krotka

---
+++ bonus/bonus4.py
@@ -0,0 +1,8 @@
+filenames = ["1.Raw Data.txt", "2.Reports.txt", "3.Presentations.txt"]
+
+for filename in filenames:
+    filename = filename.replace('.', '-', 1)
+    print(filename)
+
+filenames1 = ("1.Raw Data.txt", "2.Reports.txt", "3.Presentations.txt")
+print(filenames1)
