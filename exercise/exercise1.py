015a66226dcbfa417088beaaf2ed9087fba16ca8
mknsrp <krawczykmaciekit@gmail.com>
2025-09-20 14:50:39 +0200

    nieznaczące zmiany

--- exercise/exercise1.py
+++ exercise/exercise1.py
@@ -6,18 +6,20 @@
 #
 # while True:
 #     name = input("What is your name? ")
-#     print(name.capitalize())
-
-word = ""
-mylist = []
-while word != "exit":
-    word = input("Enter word: ")
-    mylist.append(word.capitalize())
-
-print(mylist[:-1])
+#    print(name.capitalize())
+#
+# word = ""
+# mylist = []
+# while word != "exit":
+#     word = input("Enter word: ")
+#     mylist.append(word.capitalize())
+#
+# print(mylist[:-1])
 
 
 # for i in range(0, 10):
 #     if i % 2 == 0:
 #         print(i)
 # print("These numbers are even!")
+
+
