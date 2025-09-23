25cf1beaaec67bd18c6c4562445c305809af69f8
mknsrp <krawczykmaciekit@gmail.com>
2025-09-23 11:21:20 +0200

    Dodanie katalogu i pliku bonus6 - ćwiczenia z zapisywaniem i odczytywaniem z plików

---
+++ bonus/bonus6.py
@@ -0,0 +1,10 @@
+contents = ["Siema to jakis krociutki tekst na potrzebe nauki",
+            "Siema to drugi tekst na potrzebe nauki",
+            "Siema to trzeci tekst"]
+
+filenames = ["pierwszy.txt", "drugi.txt", "trzeci.txt"]
+
+for content, filename in zip(contents, filenames):
+    file = open(f"../bonus6/{filename}", 'w')
+    file.write(content)
+
