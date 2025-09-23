57bb1c3a1bc9754c7465fd068ae93ab6a5432168
mknsrp <krawczykmaciekit@gmail.com>
2025-09-23 12:59:29 +0200

    Modyfikacja pliku exercise4.py - ćwiczenia z plikami

--- exercise/exercise4.py
+++ exercise/exercise4.py
@@ -1,8 +1,16 @@
-filenames = ["exercise4-data/a.txt", "exercise4-data/b.txt", "exercise4-data/c.txt"]
+# filenames = ["exercise4-data/a.txt", "exercise4-data/b.txt", "exercise4-data/c.txt"]
+#
+# for filename in filenames:
+#     file = open(filename, 'r')
+#     content = file.readline()
+#     print(content)
+#
+# file = open('file.txt', 'w')
+# file.write('Siema')
+# file.close()
 
-for filename in filenames:
-    file = open(filename, 'r')
-    content = file.readline()
-    print(content)
 
-
+file = open('exercise4-data/a.txt','r')
+content = file.readlines()
+for x in content:
+    print(x.strip().split())
