637e8e5f97cf57da99fcc325138f597e328fda5e
Maciej Krawczyk <krawczykmaciekit@gmail.com>
2025-09-24 22:26:59 +0200

    Dodanie komentarza w celu podtrzymania contributions

--- bonus/bonus1.py
+++ bonus/bonus1.py
@@ -1,3 +1,6 @@
 title = input("Please enter the title of book: ")
 length = len(title)
 print(f"Title length: {length}")
+# Ten komentarz to tylko mała zmiana w celu zcomittowania do podtrzymania contributions na gicie. Sorry ale dzisiaj miałem leniwy dzień, na boksie też nie byłem
+# lecz wiem, że wróce lepszy i silniejszy. Wracam do grania w nową grę - stardew valley, polecam każdemu, na pewno spodoba się wieśniakom, którym sam jestem bo to 
+# fajny symulator farmy. Do zobaczenia !!!!
