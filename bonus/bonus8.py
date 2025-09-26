214b53197f9c4a899b76ae02ad25724c365b9cd6
mknsrp <krawczykmaciekit@gmail.com>
2025-09-26 14:07:15 +0200

    Dodanie pliku bonus8.py - dalsza nauka pracy z plikami tekstowymi; prosty program dziennika

---
+++ bonus/bonus8.py
@@ -0,0 +1,9 @@
+
+
+date = input("Enter today's date: ")
+mood = input("How do you rate your mood today form 1 to 10? ")
+thoughts = input("Let your thoughts flow:\n")
+
+with open(f"../journal/{date}.txt", 'w') as file:
+    file.write(mood + '\n\n')
+    file.write(thoughts)
