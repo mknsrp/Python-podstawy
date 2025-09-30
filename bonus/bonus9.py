37af8e07a30a8b3fa5197198e096e6d652624112
mknsrp <krawczykmaciekit@gmail.com>
2025-09-30 20:02:49 +0200

    Modyfikacja pliku tod-app.py - poznanie dictionaries

--- bonus/bonus9.py
+++ bonus/bonus9.py
@@ -1,25 +1,25 @@
 while True:
     password = input("Enter new password: ")
-    result = []
+    result = {}
     if len(password) >= 8:
-        result.append(True)
+        result["length"] = True
     else:
-        result.append(False)
+        result["length"] = False
 
     digit = False
     for i in password:
         if i.isdigit():
             digit = True
-    result.append(digit)
+    result["digits"] = digit
 
     uppercase = False
     for i in password:
         if i.isupper():
             uppercase = True
-    result.append(uppercase)
+    result["upper-case"] = uppercase
 
     print(result)
-    if all(result):
+    if all(result.values()):
         print("Strong password")
     else:
         print("Weak password")
