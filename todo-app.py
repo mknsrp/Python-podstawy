759a2781c1576fcfc078e14966cf555d3b0fa323
mknsrp <krawczykmaciekit@gmail.com>
2025-10-07 17:56:44 +0200

    Modyfikacja pliku todo-app.py - dodanie funkcji w celu skrócenia kodu

--- todo-app.py
+++ todo-app.py
@@ -1,3 +1,10 @@
+
+def get_todos():
+    with open('files/todos.txt', 'r') as file:
+        todos_local = file.readlines()
+    return todos_local
+
+
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
@@ -5,8 +12,7 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]
 
-        with open('files/todos.txt', 'r') as file:
-            todos = file.readlines()
+        todos = get_todos()
 
         todos.append(todo + "\n")
 
@@ -14,8 +20,8 @@ while True:
             file.writelines(todos)
 
     elif user_action.startswith("show"):
-        with open('files/todos.txt', 'r') as file:
-            todos = file.readlines()
+
+        todos = get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -27,8 +33,7 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            with open('files/todos.txt', 'r') as file:
-                todos = file.readlines()
+            todos = get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
@@ -43,8 +48,7 @@ while True:
         try:
             number = int(user_action[9:])
 
-            with open('files/todos.txt', 'r') as file:
-                todos = file.readlines()
+            todos = get_todos()
             index = number - 1
             removed_todo = todos[index].strip('\n')
             todos.pop(index)
