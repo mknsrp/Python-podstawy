c146babcf1188e3b7927b6c76577eb898be90640
mknsrp <krawczykmaciekit@gmail.com>
2025-10-24 22:47:17 +0200

    mDOdanie pliku funkctions - moduł z funkcjami

--- todo-app.py
+++ todo-app.py
@@ -1,16 +1,5 @@
-def get_todos(filepath='files/todos.txt'):
-    """ Reads a text file and returns the list of
-    to-do items."""
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-def write_todos(todos_arg, filepath ="files/todos.txt"):
-    """ Writes the to-do items list in the text file."""
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
-
-
+#from functions import get_todos, write_todos
+import functions
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
@@ -19,15 +8,15 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         todos.append(todo + "\n")
 
-        write_todos(todos)
+        functions.write_todos(todos)
 
     elif user_action.startswith("show"):
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -39,12 +28,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
 
-            write_todos(todos)
+            functions.write_todos(todos)
         except ValueError:
             print("Your command is not valid.")
             continue
@@ -53,12 +42,12 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = functions.get_todos()
             index = number - 1
             removed_todo = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
             message = f"Todo - {removed_todo} was removed from the list."
             print(message)
