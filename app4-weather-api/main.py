33ebacbc31fb8fb5bd7184d6baa0a8e9486c6845
mknsrp <krawczykmaciekit@gmail.com>
2026-09-04 23:56:14 +0200

    app4-weather-api: add URL endpoint for annual and all data

--- app4-weather-api/main.py
+++ app4-weather-api/main.py
@@ -22,5 +22,20 @@ def about(station, date):
     }
     return result_dictionary
 
+@app.route("/api/v1/<station>")
+def all_station_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+@app.route("/api/v1/annual/<station>/<year>")
+def annual(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
